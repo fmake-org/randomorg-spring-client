99f61a671e3f287fcbb0e7f3633e1db2164195d4
Andras Hobor <snowine@gmail.com>
2019-04-11 16:41:39 +0200

    Introducing configuration class, enabling and introducing configuration properties.

---
+++ src/main/java/org/fmake/randomorg/ClientProperties.java
@@ -0,0 +1,10 @@
+package org.fmake.randomorg;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "randomorg")
+class ClientProperties {
+    private String baseUri;
+}
