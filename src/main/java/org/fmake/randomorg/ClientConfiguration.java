b00da864049be532175a2fb5956ca21ef658f437
Andras Hobor <snowine@gmail.com>
2019-04-14 16:38:26 +0200

    Set component scanning on base package.

--- src/main/java/org/fmake/randomorg/ClientConfiguration.java
+++ src/main/java/org/fmake/randomorg/ClientConfiguration.java
@@ -3,10 +3,12 @@ package org.fmake.randomorg;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@ComponentScan("org.fmake.randomorg")
 @EnableConfigurationProperties(ClientProperties.class)
 public class ClientConfiguration {
 
